e5d63282ba389a407bcd7eea153a84f466784d11
fujun-iri <fujun-iri@360.cn>
2017-04-25 19:27:48 +0800

    initialize

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # check-script-by-jsdom
-check script by jsdom.
+
+Check script to see if certain script was introduced using jsdom.
+
+## Note
+
+- jsdom v10 and later versions won't work with node v4.x.
---
+++ test.js
@@ -0,0 +1,12 @@
+var check = require('./lib/index');
+var pattern = /https?:\/\/s\.union\.360\.cn\/\d+\.js/;
+
+check(
+  'https://yyzl.github.io/',
+  pattern,
+  4 * 1000
+).then(function (e) {
+  console.log(e);
+}).catch(function (e) {
+  console.log(e);
+});
